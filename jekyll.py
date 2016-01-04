2f3af75fe963f14f58d8175158e0060fbefdd190
alejocg20 <alejocg20@hotmail.com>
2016-01-04 18:59:54 -0500

    agrega los modulos al jekyll_templates y modifica .json

--- jekyll.py
+++ jekyll.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
+#coding: utf-8
+
 import os
 import json
 import jekyll_templates
 
 input_json_file = open('fichas.json', 'r')
 input_json_data = json.load(input_json_file)
+input_json_file.close()
 
-os.makedirs('output')
+#os.makedirs('output')
 
 for ficha in input_json_data:
     output_html = jekyll_templates.generar_ficha(ficha['mapa'])
-    output_html_file = open('output/' + ficha['nombre'] + '.html', 'w')
-    output_html_file.write(output_html)
+    output_html_file = open('cristian_folder/' + ficha['nombre'] + '.html', 'w')
+    output_html_file.write(output_html.encode('utf-8'))
     output_html_file.close()
