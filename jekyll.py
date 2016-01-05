70ddf964ae61a5fe9f9b57233d50bc0913142081
Juan Leon <juansele@gmail.com>
2016-01-05 16:26:41 -0500

    reorganiza directorios y cambia nombres

--- jekyll.py
+++ jekyll.py
@@ -9,10 +9,10 @@ input_json_file = open('fichas.json', 'r')
 input_json_data = json.load(input_json_file)
 input_json_file.close()
 
-#os.makedirs('output')
+# os.makedirs('output')
 
 for ficha in input_json_data:
     output_html = jekyll_templates.generar_ficha(ficha['mapa'])
-    output_html_file = open('cristian_folder/' + ficha['nombre'] + '.html', 'w')
+    output_html_file = open('output/' + ficha['nombre'] + '.html', 'w')
     output_html_file.write(output_html.encode('utf-8'))
     output_html_file.close()
