0b228d698640e25c2b4a52194d5af5a03d60d0a1
cristian012 <camilos94-25@hotmail.com>
2016-06-01 14:29:21 -0500

    Se termina la ficha de la Y900

--- gamer_premium/js/main.js
+++ gamer_premium/js/main.js
@@ -41,11 +41,16 @@
 			 	var $AnimatedFourth = $('.s4 .mett__cont-base-flex .mett__animated');
 			 	var $AnimatedFifth = $('.s5 .mett__cont-base-flex .mett__animated');
 				var $AnimatedSixth = $('.s6 .mett__cont-base-flex .mett__animated');
+				var $AnimatedSeventh = $('.s7 .mett__cont-base-flex .mett__animated');
+				var $AnimatedEight = $('.s8 .mett__cont-base-flex .mett__animated');
+				var $AnimatedNineht = $('.s9 .mett__cont-base-flex .mett__animated');
+				var $AnimatedTenth = $('.s10 .mett__cont-base-flex .mett__animated');
+				var $AnimatedEleventh = $('.s11 .mett__cont-base-flex .mett__animated');
 
 			 	 $('#fullpage').fullpage({
 
 					menu: '#mainMenu',
-					anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
+					anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eightPage', 'ninePage', 'tenthPage', 'eleventhPage'],
 					navigationTooltips: ['Generalidades', 'Descripción', 'Especificaciones', 'Cambios y devoluciones'],
 					navigation: true,
 					onLeave: function(index, nextIndex, direction){
@@ -54,34 +59,59 @@
 					if( index == 1 && nextIndex == 2 ) { 
 
 						$AnimatedSecond.addClass('animated fadeInUp fadeIn');
-						$AnimatedSecond.eq(0).css('animation-delay', '.1s');
-						$AnimatedSecond.eq(1).css('animation-delay', '.2s');
-						$AnimatedSecond.eq(2).css('animation-delay', '.3s');
 			 			// $isAnimatedSecondSingle.addClass('animated rollIn').css('animation-delay', '1.7s');
 
 
 						} else if ( ( index == 1 || index == 2) && nextIndex == 3 ) {
 							
 							$AnimatedThird.eq(0).addClass('animated fadeInLeft fadeIn');
+							
 							$AnimatedThird.eq(1).addClass('animated fadeInRight fadeIn');
 
 			 			} else if ( ( index == 1 || index == 2 || index == 3) && nextIndex == 4 ) {
 
 							$AnimatedFourth.eq(0).addClass('animated fadeInRight fadeIn');
+							
 							$AnimatedFourth.eq(1).addClass('animated fadeInRight fadeIn');
 
 						} else if ( ( index == 1 || index == 2 || index == 3 || index == 4) && nextIndex == 5 ) {
 
 							$AnimatedFifth.eq(0).addClass('animated fadeInUp fadeIn');
+
 							$AnimatedFifth.eq(1).addClass('animated fadeInLeft fadeIn');
+
 			 				$AnimatedFifth.eq(1).css('animation-delay', '.3s');
+
 							$AnimatedFifth.eq(2).addClass('animated fadeInRight fadeIn');
+
 							$AnimatedFifth.eq(2).css('animation-delay', '.6s');
 
 						} else if ( ( index == 1 || index == 2 || index == 3 || index == 4 || index == 5) && nextIndex == 6 ) {
 
 			 				$AnimatedSixth.addClass('animated fadeInLeft fadeIn');
+
+			 			} else if ( ( index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 ) && nextIndex == 7 ) {
+
+			 				$AnimatedSeventh.addClass('animated fadeInUp fadeIn');
+
+			 			} else if ( ( index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7 ) && nextIndex == 8 ) {
+			 			
+			 				$AnimatedEight.addClass('animated fadeInRight fadeIn');
+
+			 			} else if ( ( index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7  || index == 8) && nextIndex == 9 ) {
+			 				
+			 				$AnimatedNineht.addClass('animated fadeInLeft fadeIn');
+			 			
+			 			} else if ( ( index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7  || index == 8 || index == 9) && nextIndex == 10 ) {
+			 				
+			 				$AnimatedTenth.addClass('animated fadeInRight fadeIn');
+
+			 			} else if ( ( index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7  || index == 8 || index == 9 || index == 10) && nextIndex == 11 ) {
+			 				
+			 				$AnimatedEleventh.addClass('animated fadeInLeft fadeIn');
 			 			}
+
+
 			 		}
 				});
 			  });
