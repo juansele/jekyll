81aa2d4ed7be192f40aa346756c87e5bc30f3db1
cristian012 <camilos94-25@hotmail.com>
2016-06-01 09:07:08 -0500

    Trabajo en ficha de Y900 IDEPAD

--- gamer_premium/js/main.js
+++ gamer_premium/js/main.js
@@ -29,63 +29,63 @@
 		
 		 
 
-		//if (screen && screen.width > 768) {
+		if (screen && screen.width > 768) {
 
-			// document.write('<link rel="stylesheet" type="text/css" href="lib/bower_components/fullpage.js/dist/jquery.fullpage.min.css">')
-			// document.write('<script type="text/javascript" src="lib/bower_components/fullpage.js/dist/jquery.fullpage.js"><\/script>');
+			 document.write('<link rel="stylesheet" type="text/css" href="lib/bower_components/fullpage.js/dist/jquery.fullpage.min.css">')
+			 document.write('<script type="text/javascript" src="lib/bower_components/fullpage.js/dist/jquery.fullpage.js"><\/script>');
 
-			// $(document).ready(function() {
+			 $(document).ready(function() {
 
-			// 	var $AnimatedSecond = $('.s2 .mett__cont-base-flex .mett__animated');
-			// 	var $AnimatedThird = $('.s3 .mett__cont-base-flex .mett__animated');
-			// 	var $AnimatedFourth = $('.s4 .mett__cont-base-flex .mett__animated');
-			// 	var $AnimatedFifth = $('.s5 .mett__cont-base-flex .mett__animated');
-			// 	var $AnimatedSixth = $('.s6 .mett__cont-base-flex .mett__animated');
+			 	var $AnimatedSecond = $('.s2 .mett__cont-base-flex .mett__animated');
+			 	var $AnimatedThird = $('.s3 .mett__cont-base-flex .mett__animated');
+			 	var $AnimatedFourth = $('.s4 .mett__cont-base-flex .mett__animated');
+			 	var $AnimatedFifth = $('.s5 .mett__cont-base-flex .mett__animated');
+				var $AnimatedSixth = $('.s6 .mett__cont-base-flex .mett__animated');
 
-			// 	 $('#fullpage').fullpage({
+			 	 $('#fullpage').fullpage({
 
-			// 		menu: '#mainMenu',
-			// 		anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
-			// 		navigationTooltips: ['Generalidades', 'Descripción', 'Especificaciones', 'Cambios y devoluciones'],
-			// 		navigation: true,
-			// 		onLeave: function(index, nextIndex, direction){
+					menu: '#mainMenu',
+					anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
+					navigationTooltips: ['Generalidades', 'Descripción', 'Especificaciones', 'Cambios y devoluciones'],
+					navigation: true,
+					onLeave: function(index, nextIndex, direction){
 							
 
-			// 		if( index == 1 && nextIndex == 2 ) { 
+					if( index == 1 && nextIndex == 2 ) { 
 
-			// 			$AnimatedSecond.addClass('animated fadeInUp fadeIn');
-			// 			$AnimatedSecond.eq(0).css('animation-delay', '.1s');
-			// 			$AnimatedSecond.eq(1).css('animation-delay', '.2s');
-			// 			$AnimatedSecond.eq(2).css('animation-delay', '.3s');
-			// 			// $isAnimatedSecondSingle.addClass('animated rollIn').css('animation-delay', '1.7s');
+						$AnimatedSecond.addClass('animated fadeInUp fadeIn');
+						$AnimatedSecond.eq(0).css('animation-delay', '.1s');
+						$AnimatedSecond.eq(1).css('animation-delay', '.2s');
+						$AnimatedSecond.eq(2).css('animation-delay', '.3s');
+			 			// $isAnimatedSecondSingle.addClass('animated rollIn').css('animation-delay', '1.7s');
 
 
-			// 			} else if ( ( index == 1 || index == 2) && nextIndex == 3 ) {
+						} else if ( ( index == 1 || index == 2) && nextIndex == 3 ) {
 							
-			// 				$AnimatedThird.eq(0).addClass('animated fadeInLeft fadeIn');
-			// 				$AnimatedThird.eq(1).addClass('animated fadeInRight fadeIn');
+							$AnimatedThird.eq(0).addClass('animated fadeInLeft fadeIn');
+							$AnimatedThird.eq(1).addClass('animated fadeInRight fadeIn');
 
-			// 			} else if ( ( index == 1 || index == 2 || index == 3) && nextIndex == 4 ) {
+			 			} else if ( ( index == 1 || index == 2 || index == 3) && nextIndex == 4 ) {
 
-			// 				$AnimatedFourth.eq(0).addClass('animated fadeInRight fadeIn');
-			// 				$AnimatedFourth.eq(1).addClass('animated fadeInRight fadeIn');
+							$AnimatedFourth.eq(0).addClass('animated fadeInRight fadeIn');
+							$AnimatedFourth.eq(1).addClass('animated fadeInRight fadeIn');
 
-			// 			} else if ( ( index == 1 || index == 2 || index == 3 || index == 4) && nextIndex == 5 ) {
+						} else if ( ( index == 1 || index == 2 || index == 3 || index == 4) && nextIndex == 5 ) {
 
-			// 				$AnimatedFifth.eq(0).addClass('animated fadeInUp fadeIn');
-			// 				$AnimatedFifth.eq(1).addClass('animated fadeInLeft fadeIn');
-			// 				$AnimatedFifth.eq(1).css('animation-delay', '.3s');
-			// 				$AnimatedFifth.eq(2).addClass('animated fadeInRight fadeIn');
-			// 				$AnimatedFifth.eq(2).css('animation-delay', '.6s');
+							$AnimatedFifth.eq(0).addClass('animated fadeInUp fadeIn');
+							$AnimatedFifth.eq(1).addClass('animated fadeInLeft fadeIn');
+			 				$AnimatedFifth.eq(1).css('animation-delay', '.3s');
+							$AnimatedFifth.eq(2).addClass('animated fadeInRight fadeIn');
+							$AnimatedFifth.eq(2).css('animation-delay', '.6s');
 
-			// 			} else if ( ( index == 1 || index == 2 || index == 3 || index == 4 || index == 5) && nextIndex == 6 ) {
+						} else if ( ( index == 1 || index == 2 || index == 3 || index == 4 || index == 5) && nextIndex == 6 ) {
 
-			// 				$AnimatedSixth.addClass('animated fadeInLeft fadeIn');
-			// 			}
-			// 		}
-			// 	});
-			//  });
-		//} else if (screen && screen.width < 768){
+			 				$AnimatedSixth.addClass('animated fadeInLeft fadeIn');
+			 			}
+			 		}
+				});
+			  });
+		} else if (screen && screen.width < 768){
 			document.write('<link rel="stylesheet" type="text/css" href="css/style-nofullpage.css">')
-		//}
+		}
 		
